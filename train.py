24721119e56d18923a8597b416e57d4e5ae5ce73
tugcetunalilar <33204902+tugcetunalilar@users.noreply.github.com>
2021-01-16 18:37:29 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -10,19 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +37,21 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df,y_df
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+ds = TabularDatasetFactory.from_delimited_files(['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'])
+
+x, y = clean_data(ds)
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=0)
+
+### YOUR CODE HERE ###a
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
